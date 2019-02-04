66a752e0e16e31e0ff27ad1e9debdda3c0838d6f
Jaela Wesley <jcwesley93@gmail.com>
2019-02-04 13:58:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ colors = ["orange", "yellow", "blue", "green"]
 end
 
 def add_element_to_end_of_array(array, element)
-numbers.push(5)
+colors = ["orange", "yellow", "blue", "green"]
+colors.push("red")
 end
 
 def add_element_to_start_of_array(array, element)
